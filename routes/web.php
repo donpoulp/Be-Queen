dbd0c2c4941a4438b06cf9af3729ee6bd9c6bcfa
Adonat74 <donatgoninet.antoine@gmail.com>
2024-07-10 16:00:26 +0200

    routing category simple

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/product', function () {
-    return 'liste de produit';
+Route::get('/category', function () {
+    return 'liste des categories';
 });
 
-Route::get('/product/{id}', function (int $id) {
-    return 'produit' .$id;
+Route::get('/category/{id}', function ($id) {
+    return 'category' . $id;
 });
 
-Route::get('/customer', function () {
-    return 'liste de client';
-});
 
-Route::get('/customer/{id}', function (int $id) {
-    return 'client' .$id;
-});
