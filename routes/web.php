bec8b342ff5722abc0e92c43e3b29ba6ffcbacfa
donpoulp <raphael.petrozzi@le-campus-numerique.fr>
2024-07-10 14:46:30 +0200

    New - fin etape 1 creation des routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product', function () {
+    return 'liste de produit';
+});
+
+Route::get('/product/{id}', function (int $id) {
+    return 'produit' .$id;
+});
+
+Route::get('/customer', function () {
+    return 'liste de client';
+});
+
+Route::get('/customer/{id}', function (int $id) {
+    return 'client' .$id;
+});
