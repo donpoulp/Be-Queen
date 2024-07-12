8203d76fbfcd129729603ab336b7ca602bbdf74c
Adonat74 <donatgoninet.antoine@gmail.com>
2024-07-12 14:12:30 +0200

    added a migration file

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class OrderController extends Controller
 {
     public function displaySingleOrder(string $id): object {
---
+++ database/migrations/2024_07_12_074025_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('id_orders');
+            $table->string('status', length: 45);
+            $table->timestamp('created_at');
+            $table->foreignId('users_id');
+            $table->foreignId('bikes_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
