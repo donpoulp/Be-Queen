231ae6424f80dc92dcdbbed8ae7ee308bc25bbe2
Tristan Chadeuf <tristan.chadeuf@le-campus-numerique.fr>
2024-07-12 11:11:10 +0200

    creation de route et controleur customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,22 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\View\View;
 
 class CustomerController extends Controller
 {
-    public function customershow(){
-        return response()
-
-    }
-
-    public function customershowid($id)
+    public function customershowid(string $id):object
     {
         return response()->json([
+            'id'=>$id,
+            'name'=>'Don diego',
 
-            'name' => 'Abigail',
-            'adresse' => 'rue blabla',
+            ]
 
+        );
+    }
+
+    public function customershow():object
+    {
+        return response()->json([
+            'client'=>'Don diego',
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,10 @@
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/product', function () {
-    return 'liste de produit';
+Route::get('/', function(){
+    return view('welcome');
 });
 
-Route::get('/product/{id}', function (int $id) {
-    return 'produit' .$id;
-});
 
 Route::get('/customer', [CustomerController::class, 'customershow'] );
 
