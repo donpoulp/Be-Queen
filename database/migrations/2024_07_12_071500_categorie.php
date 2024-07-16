586fc41f0351deaadb9088510ad587658147b3b5
Tristan Chadeuf <tristan.chadeuf@le-campus-numerique.fr>
2024-07-16 10:40:22 +0200

    rename migrations user

--- database/migrations/2024_07_12_071500_categorie.php
+++ database/migrations/2024_07_12_071500_categorie.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categorie', function (Blueprint $table) {
-            $table->id()->primary();
+            $table->id();
             $table->string('name');
             $table->string('description');
             $table->timestamps();
